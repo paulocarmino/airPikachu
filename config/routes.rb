995a721ada7b370893af412ea29e56658619710a
Paulo Carmino <contato@paulocarmino.com>
2017-08-08 17:16:22 -0300

    Configured gravatar helper and fix password placeholder in sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
+  root to: "pages#home"
   devise_for :users
-  root to: "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
