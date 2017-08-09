2797a0c9fb08b81f2a78d67c217932432c703117
Paulo Carmino <contato@paulocarmino.com>
2017-08-09 13:16:04 -0300

    Styling and edit profile pages (Show and Edit)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  
+
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
-             controllers: {omniauth_callbacks: 'omniauth_callbacks'}
+             controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  resources :users, only: [:show]
 end
