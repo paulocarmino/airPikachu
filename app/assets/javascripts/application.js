ed850c1c5a49ceeaec581d5acaba3152e3d18c0e
Paulo Carmino <contato@paulocarmino.com>
2017-09-18 09:46:04 -0300

    Create remove Photo function with Ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,18 +15,3 @@
 //= require jquery_ujs
 //= require toastr
 //= require_tree .
-
-toastr.options = ({
- “closeButton”: true,
- “debug”: false,
- “positionClass”: “toast-bottom-right”,
- “onclick”: null,
- “showDuration”: “300”,
- “hideDuration”: “1500”,
- “timeOut”: “5000”,
- “extendedTimeOut”: “1000”,
- “showEasing”: “swing”,
- “hideEasing”: “linear”,
- “showMethod”: “fadeIn”,
- “hideMethod”: “fadeOut”
- });
