efa033cf7c0719ecca042b183f0954f486cb422b
Paulo Carmino <contato@paulocarmino.com>
2017-11-07 12:42:59 -0200

    Add room index page with listings

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,4 +8,11 @@ class Room < ApplicationRecord
   validates :bed_room, presence: true
   validates :bath_room, presence: true
 
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "blank.jpg"
+    end
+  end
 end
