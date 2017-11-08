a41ed898f3f9dad715aab5a5847325fe0b9bfa84
Paulo Carmino <contato@paulocarmino.com>
2017-11-08 14:18:40 -0200

    Add near by in room show page

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
